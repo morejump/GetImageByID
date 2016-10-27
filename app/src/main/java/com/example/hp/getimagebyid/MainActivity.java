398ebd00a77d6fd30fa089857d2d1407ec1c8dc0
DESKTOP-V5C0MBO\HP <luuvanthaoepu@gmail.com>
2016-10-28 01:15:23 +0700

    uncomment to see how this workds

--- app/src/main/java/com/example/hp/getimagebyid/MainActivity.java
+++ app/src/main/java/com/example/hp/getimagebyid/MainActivity.java
@@ -9,13 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        public static int getNavigationHeight(Context context) {
-//            Resources resources = context.getResources();
-//            int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
-//            if (resourceId > 0) {
-//                return resources.getDimensionPixelSize(resourceId);
-//            }
-//            return 0;
-//        }
+//        getResources().getIdentifier(prefix + i,"raw", rootView.getContext().getPackageName())
     }
 }
